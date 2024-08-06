77e312132842d533179e1a041007a516809e79f3
CYJ <wjdtpdbs1887@naver.com>
2024-08-06 17:14:21 +0900

    DI 코드수정

--- src/main/java/com/tenco/bank/service/UserService.java
+++ src/main/java/com/tenco/bank/service/UserService.java
@@ -14,24 +14,21 @@ import com.tenco.bank.repository.interfaces.UserRepository;
 @Service // IoC 대상 (싱글톤으로 관리)
 public class UserService {
 
-	// DI - 의존 주입
-	@Autowired
 	private UserRepository userRepository;
-
-//	@Autowired 어노테이션으로 대체 가능하다.
-//	생존자 의존 주입 - DI
-//	public UserService(UserRepository userRepository) {
-//		this.userRepository = userRepository;
-//	}
+	
+	// @Autowired 어노테이션으로 대체 가능 하다.
+	// 생존자 의존 주입 - DI
+	@Autowired // 어노테이션으로 대체 가능하다.
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	/**
 	 * 회원 등록 서비스 기능 트랜잭션 처리
-	 * 
+	 * 트랜잭션 처리
 	 * @param dto
 	 */
-
-	// 회원 가입 처리
-	@Transactional
+	@Transactional  // 트랜잭션 처리는 반드시 습관화
 	public void createUser(SignUpDTO dto) {
 		int result = 0;
 
