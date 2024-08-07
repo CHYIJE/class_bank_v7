452740c37829458c0989c082063db8c3f23ed3ec
CYJ <wjdtpdbs1887@naver.com>
2024-08-07 11:17:38 +0900

    로그아웃 기능 추가

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -7,20 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.repository.model.User;
 import com.tenco.bank.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller // Ioc의 대상 (싱글톤 패턴으로 관리 됨)
 @RequestMapping("/user") // 대문 처리
 public class UserController {
 
 	private UserService userService;
+	private final HttpSession session;
 
 	// DI처리
 	@Autowired // 노란색 경고는 사용할 필요 없음 - 가독성 위해서 선언해도 됨
-	public UserController(UserService service) {
+	public UserController(UserService service, HttpSession session) {
 		this.userService = service;
+		this.session = session;
 	}
 
 	/**
@@ -31,7 +37,7 @@ public class UserController {
 
 	@GetMapping("/sign-up")
 	public String signUpPage() {
-		
+
 		// prefix: WEB-INF/view
 		// suffix: .jsp
 		// return: user/signUp
@@ -65,7 +71,51 @@ public class UserController {
 		// 서비스 객체 전달
 		userService.createUser(dto);
 		// TODO - 추후 수정
+		return "redirect:/user/sign-in";
+	}
+
+	/*
+	 * 로그인 화면 요청 주소설계 : http://localhost:8080/user/sign-in
+	 */
+	@GetMapping("/sign-in")
+	public String signInPage() {
+		// 인증검사 x
+		// 유효성 검사 x
+		return "user/signIn";
+	}
+
+	/*
+	 * 
+	 */
+	@PostMapping("/sign-in")
+	public String signProc(SignInDTO dto) {
+
+		// 1. 인증 검사 x
+		// 2. 유효성 검사
+		if (dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException("username을 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+
+		if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException("password을 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+
+		// 서비스 호출
+		User principal = userService.readUser(dto);
+
+		// 세션 메모리에 등록 처리
+		session.setAttribute("principal", principal);
+
+		// 새로운 페이지로 이동처리
+		// TODO - 계좌 목록 페이지 이동처리 예정
 		return "redirect:/index";
+	}
+	
+	// 코드 추가
+	@GetMapping("/logout")
+	public String logout() {
+		session.invalidate();  // 로그아웃 됨
+		return "redirect:/user/sign-in";
 	}
 
 }
