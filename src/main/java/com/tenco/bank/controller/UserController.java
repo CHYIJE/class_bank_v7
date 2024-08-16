448feb8a8792e1f1a132eded801cca3bb7f94f90
CYJ <wjdtpdbs1887@naver.com>
2024-08-16 11:49:08 +0900

    카카오 로그인

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.tenco.bank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.tenco.bank.dto.KakaoProfile;
 import com.tenco.bank.dto.OAuthToken;
 import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
@@ -39,6 +41,9 @@ public class UserController {
 		this.userService = service;
 		this.session = session;
 	}
+	
+	@Value("${tenco.key}")
+	private String kencoKey;
 
 	/**
 	 * 회원 가입 페이지 요청 주소설계 -> http://localhost:8080/user/sign-up
@@ -134,7 +139,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/kakao")
-	@ResponseBody //@RestController = @Controller + @ ResponseBody
+	// @ResponseBody //@RestController = @Controller + @ ResponseBody
 	public String getMethodName(@RequestParam("code") String code) {
 		System.out.println(code);
 		
@@ -173,12 +178,46 @@ public class UserController {
 		HttpEntity<MultiValueMap<String, String>> reqKakoInfoMessage = new HttpEntity<>(headers2);
 		
 		// 통신 요청
-		ResponseEntity<String> response2 = rt2.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST,
-				reqKakoInfoMessage, String.class);
+		ResponseEntity<KakaoProfile> response2 = 
+				rt2.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST,
+				reqKakoInfoMessage, KakaoProfile.class);
 		
-		System.out.println("response2 : " + response2);
+		KakaoProfile kakaoProfile = response2.getBody();
 		
-		return response2.getBody();
+		System.out.println("kakaoProfile : " + kakaoProfile);
+		// --- 카카오 사용자 정보 응답 완료 ---- 
+		
+		// 최초 사용자라면 자동 회원 가입 처리 (우리 서버)
+		// 회원가입 이력이 있는 사용자라면 바로 세션 처리(우리 서버)
+		// 사전기반 -- 소셜 사용자는 비밀번호를 입력하는가? 안하는가?
+		// 우리서버에 회원가입시에 --> password -> not null (무조건 만들어 넣어야 함 DB정책)
+		
+		SignUpDTO signUpDTO = SignUpDTO.builder()
+						.username(kakaoProfile.getProperties().getNickname() 
+								+ "_" + kakaoProfile.getId())
+						.fullname("OAuth_" + kakaoProfile.getProperties().getNickname())
+						.password(kencoKey)
+						.build();
+		
+		// 2. 우리 사이트 최초 소셜 사용자 인지 판별
+		User oldUser = userService.searchUsername(signUpDTO.getUsername());
+		if(oldUser == null) {
+			// 사용자가 최초 소셜 로그인 사용자임
+			oldUser = new User();
+			oldUser.setUsername(signUpDTO.getUsername());
+			oldUser.setPassword(null);
+			oldUser.setFullname(signUpDTO.getFullname());
+			// 프로필 이미지 여부에 따라 조건식 추가
+			signUpDTO.setOriginFileName(kakaoProfile.getProperties().getThumbnailImage());
+			userService.createUser(signUpDTO);
+		}
+		
+		// 자동 로그인 처리
+		session.setAttribute(Define.PRINCIPAL, oldUser);
+		
+		
+		
+		return "redirect:/account/list";
 	}
 	
 	
